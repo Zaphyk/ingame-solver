e40530a405515957337f41e1c9b37892123f3c61
Zaphyk <zaphyk@live.com>
2018-10-06 21:18:59 -0300

    add tests for the trivia

--- trivia.py
+++ trivia.py
@@ -1,8 +1,32 @@
 
 class Guesser():
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def guess(question: str, options: list) -> int:
-		pass
+    def guess(question: str, options: list) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    trivia = Guesser()
+    def assert_guess(question: str, options: list, answer: int):
+        assert(trivia.guess(question, options))
+
+    assert_guess(
+        '¿Cuál de estos deportistas argentinos fue dos veces abanderado Olímpico?',
+        ["Luciana Aymar","Carlos Espínola","Juan Curuchet"],
+        1
+    )
+
+    assert_guess(
+        '¿Qué libro fue prohibido durante la dictadura militar argentina?',
+        ["La Metamorfosis","El Principito","Moby Dick"],
+        1
+    )
+
+    assert_guess(
+        '¿Dónde se encuentra el mejor restaurante de Latinoamérica según "Chefs\' Choice Award"?',
+        ["Lima, Perú","Bogotá, Colombia","Buenos Aires, Argentina"],
+        1
+    )
